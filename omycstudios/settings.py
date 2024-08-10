3228af2b922f55126fcd465225522b286b186916
Francis Antwi <66670804+Sheamusticals@users.noreply.github.com>
2024-08-10 03:42:03 -0800

    Update settings.py

--- omycstudios/settings.py
+++ omycstudios/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#rrw0y)1w^b6-#obcb2gb0a22frpw9mdbg2o@kbwc^jp6)epmy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
