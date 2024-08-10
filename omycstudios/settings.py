1f6df0f0e0b12b5a6d50a2c731e79d79ea661ef0
Francis Antwi <sheamusticals@gmail.com>
2024-08-10 11:59:07 -0800

    new changes

--- omycstudios/settings.py
+++ omycstudios/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#rrw0y)1w^b6-#obcb2gb0a22frpw9mdbg2o@kbwc^jp6)epmy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
